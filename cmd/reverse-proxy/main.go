a16593535a6e91a6e33381723528368c11bcd87a
eru <eru0@pm.me>
2019-03-03 19:40:02 +0100

    .

--- cmd/reverse-proxy/main.go
+++ cmd/reverse-proxy/main.go
@@ -1,44 +1,42 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"os/signal"
 
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strconv"
 	"time"
 )
 
-var (
-	_nHosts int
-	_port   int
-	count   int
-)
+type Config struct {
+	Hosts []struct {
+		Path string
+		Host string
+	}
+}
 
 func main() {
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT variable is not set")
 	}
-	var err error
-	_port, err = strconv.Atoi(port)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	nHosts := os.Getenv("HOSTS")
-	if nHosts == "" {
-		log.Fatal("HOSTS variable is not set")
-	}
-	_nHosts, err = strconv.Atoi(nHosts)
+	configFile, err := os.Open("config.json")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
-	count = 0
+	defer configFile.Close()
+	bytes, _ := ioutil.ReadAll(configFile)
+	var config Config
+	json.Unmarshal(bytes, &config)
 
 	tr := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
@@ -53,33 +51,45 @@ func main() {
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
-	pxs := initProxy(tr)
 
-	http.HandleFunc("/api/", func(res http.ResponseWriter, req *http.Request) {
-		count = (count + 1) % _nHosts
-		pxs[count].ServeHTTP(res, req)
+	r := http.NewServeMux()
+
+	r.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("I'm OK!\n"))
 	})
 
-	if err = http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
+	for _, host := range config.Hosts {
+		r.HandleFunc(host.Path, func(res http.ResponseWriter, req *http.Request) {
+			log.Println(req.RemoteAddr, req.Method, req.URL.String())
+			urlstr := host.Host
+			url, err := url.Parse(urlstr)
+			if err != nil {
+				log.Fatal("Error parsing URLs ", err)
+			}
+			proxy := httputil.NewSingleHostReverseProxy(url)
+			proxy.Transport = tr
+			proxy.ServeHTTP(res, req)
+		})
 	}
-}
 
-func initProxy(tr *http.Transport) []*httputil.ReverseProxy {
-	prxs := []*httputil.ReverseProxy{}
-	for i := 1; i <= _nHosts; i++ {
-		p := (_port + i) % 65536
-		if p < 1024 {
-			p += 1024
-		}
-		urlstr := "http://localhost" + ":" + strconv.Itoa(p)
-		url, err := url.Parse(urlstr)
-		if err != nil {
-			log.Fatal("Error parsing URLs ", err)
-		}
-		proxy := httputil.NewSingleHostReverseProxy(url)
-		proxy.Transport = tr
-		prxs = append(prxs, proxy)
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: r,
 	}
-	return prxs
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+		fmt.Println()
+		log.Println("SIGINT captured. Closing server.")
+		err := server.Shutdown(context.Background())
+		if err != nil {
+			log.Printf("HTTP server Shutdown with error %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening at :%s", port)
+	log.Println(server.ListenAndServe())
+
 }
